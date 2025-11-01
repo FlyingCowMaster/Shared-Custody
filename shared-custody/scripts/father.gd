49cb4a6ab6da4689fb0e7e70448c35bd9cd2bff9
Joseph Rosso <jdr1652@rit.edu>
2025-11-01 03:45:17 -0400

    fixed merge conflicts, inclued mother and father + child. my will to live is increadably strong rn :D

--- shared-custody/scripts/father.gd
+++ shared-custody/scripts/father.gd
@@ -11,13 +11,13 @@ var target_velocity = Vector3.ZERO
 func _physics_process(delta):
 	var direction = Vector3.ZERO
 
-	if Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed("move_right2"):
 		direction.x += 1
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("move_left2"):
 		direction.x -= 1
-	if Input.is_action_pressed("move_down"):
+	if Input.is_action_pressed("move_down2"):
 		direction.z += 1
-	if Input.is_action_pressed("move_up"):
+	if Input.is_action_pressed("move_up2"):
 		direction.z -= 1
 
 	if direction != Vector3.ZERO:
