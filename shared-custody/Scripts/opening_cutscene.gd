cc95c086f10f535321f248c78cf332855d8b057a
Jaiden Johnson <jjaidenj09@gmail.com>
2025-11-02 14:57:32 -0500

    done

--- shared-custody/Scripts/opening_cutscene.gd
+++ shared-custody/Scripts/opening_cutscene.gd
@@ -6,4 +6,4 @@ func _ready() -> void:
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	get_tree().change_scene_to_file("res://scenes/title_screen.tscn") # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/level1.tscn") # Replace with function body.
