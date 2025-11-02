3ed74577d8e25d623d9050ab81a7f1a6d4fb4859
Joseph Rosso <jdr1652@rit.edu>
2025-11-01 22:22:48 -0400

    added door that both parents can posses

---
+++ shared-custody/scripts/door.gd
@@ -0,0 +1,15 @@
+extends StaticBody3D
+
+@export var isPossesedMom = false
+@export var isPossesedDad = false
+
+func togglePossesionMom():
+	isPossesedMom = !isPossesedMom
+
+func togglePossesionDad():
+	isPossesedDad = !isPossesedDad
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta) -> void:
+	if isPossesedDad and isPossesedMom:
+		print("Shared Possesion!")  
