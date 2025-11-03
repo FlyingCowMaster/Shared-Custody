74286a14b7f5c2177e08a2154979b6a7fb6ae347
Jgouken <jjaidenj09@gmail.com>
2025-11-03 14:29:36 -0500

    Organization & Cutscene Skip

--- shared-custody/Scripts/opening_cutscene.gd
+++ shared-custody/Scripts/opening_cutscene.gd
@@ -3,7 +3,10 @@ extends Node2D
 func _ready() -> void:
 	$AnimationPlayer.play("OpeningCutscene")
 
+func _process(_delta: float) -> void:
+	if Input.is_anything_pressed():
+		get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
+		# The cutscene is now skippable
 
-
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	get_tree().change_scene_to_file("res://scenes/level1.tscn") # Replace with function body.
+func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
+	get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
