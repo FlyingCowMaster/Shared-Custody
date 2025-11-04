dbfb92250dbb21534fe4c747a09dfd7e9ccf37d0
Joseph Rosso <jdr1652@rit.edu>
2025-11-04 16:35:27 -0500

    forgor to commit all changes from last commit

--- shared-custody/scripts/light_bulb.gd
+++ shared-custody/scripts/light_bulb.gd
@@ -6,25 +6,22 @@ var light
 
 func _ready():
 	light = $WALL_LAMP_2/OmniLight3D
+	light.visible = false
 
 func togglePossesionMom():
 	set_collision_layer_value(2,isPossesed)
 	isPossesed = !isPossesed
+	toggleLight()
 
 func togglePossesionDad():
 	set_collision_layer_value(3,isPossesed)
 	isPossesed = !isPossesed
+	toggleLight()
 	
-func _process(_delta: float) -> void:
-	if (isPossesed): turn_on_light()
-	else: turn_off_light()
-
-func turn_on_light():
-	light.visible = true
-	set_collision_layer_value(5, false)
-	#print("Going")
-
-func turn_off_light():
-	light.visible = false
-	set_collision_layer_value(5, true)
-	#print("Stopping")
+func toggleLight():
+	if (isPossesed): 
+		light.visible = true
+		set_collision_layer_value(5, false)
+	else:
+		light.visible = false
+		set_collision_layer_value(5, true)
