c5d8a29da1347bdf9655e5b652df5a7f1b7bccf3
Jgouken <jjaidenj09@gmail.com>
2025-11-04 15:08:29 -0500

    Camera Controlller
    An intricuit* detailed* and perfect* camera controller for the game, fit with hard-coded values and everything!

--- shared-custody/Scripts/hole.gd
+++ shared-custody/Scripts/hole.gd
@@ -5,13 +5,13 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
-func _on_area_3d_body_entered(body: Node3D) -> void:
+func _on_area_3d_body_entered(_body: Node3D) -> void:
 	pass
 
-func _on_area_shape_entered(area_rid: RID, area: Area3D, area_shape_index: int, local_shape_index: int) -> void:
+func _on_area_shape_entered(_area_rid: RID, area: Area3D, _area_shape_index: int, _local_shape_index: int) -> void:
 	print(area.name + ' entered my tight hole.')
 	print(area.get_parent_node_3d().name)
 	if area.get_parent_node_3d().is_in_group('Box'):
