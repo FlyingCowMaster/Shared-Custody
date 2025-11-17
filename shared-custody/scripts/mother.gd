30d3ce91a42090c46d71d51520bc141b8f6f2bb0
Jaiden Johnson <jjaidenj09@gmail.com>
2025-11-17 15:44:59 -0500

    Some Fixes
    - Mom and Dad rotate towards walking direction.
    - Particles elongated to further show pathing.

--- shared-custody/scripts/mother.gd
+++ shared-custody/scripts/mother.gd
@@ -30,8 +30,12 @@ func _physics_process(delta):
 			direction.z -= 1
 
 		if direction != Vector3.ZERO:
-			direction = direction.normalized()
 			# Setting the basis property will affect the rotation of the node.
+			direction = direction.normalized()
+			
+			# Rotate the character, as long as they are moving
+			var target_angle = atan2(-direction.x, -direction.z)
+			rotation.y = lerp_angle(rotation.y, target_angle, delta * 10)
 
 		# Ground Velocity
 		target_velocity.x = direction.x * speed
@@ -40,10 +44,11 @@ func _physics_process(delta):
 		# Vertical Velocity
 		if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
 			target_velocity.y = target_velocity.y - (fall_acceleration * delta)
-
+		
 		# Moving the Character
 		velocity = target_velocity
 		move_and_slide()
+		
 	if Input.is_action_pressed("possesion") and canPosess:
 		canPosess = false
 		if not isPossesing:
